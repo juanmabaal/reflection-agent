900a136ad94326a2065b5b6afd47ffd5b13740e8
Juan Manuel Balaguera <juanma.baal@gmail.com>
2026-06-03 22:06:10 -0500

    feat: implement generation and reflection chains

---
+++ chains.py
@@ -0,0 +1,32 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
+
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+  [
+    (
+      "system",
+      "You are a viral twitter influencer grading a tweet. Generate critique and recomendations for the user's tweet"
+      "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+    ),
+    MessagesPlaceholder(variable_name="messages")
+  ]  
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter tecnie influencer assistant tasked with writing excellent twitter posts."
+            "Generate the best twitter post possible for the user's request. "
+            "If the user provides critique, respond with a revised version of your previous attempts"
+        ),
+        MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+llm= ChatOpenAI()
+# llm = ChatDeepSeek()
+generate_chain = generation_prompt | llm
+reflect_chain = reflection_prompt | llm 
